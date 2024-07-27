a00e3f575144b383542e1c9fa09a3cc55c11c857
quannguyen <quanmanchester0405@gmail.com>
2024-07-27 21:42:02 +0700

    feat: intergrate google cloud hook

--- src/Home/Home_header.jsx
+++ src/Home/Home_header.jsx
@@ -1,51 +1,50 @@
-import React,{useContext, useEffect} from 'react'
-import {
- Link
-} from "react-router-dom";
-import {Nav,S_inforUser,S_btnLogOut} from'./Home_CSS'
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Nav, S_inforUser, S_btnLogOut } from "./Home_CSS";
 import { ImExit } from "react-icons/im";
 import { useNavigate } from "react-router-dom";
-import {inforUser} from '../Firebase/UserContext'
+// import { inforUser } from "../Firebase/UserContext";
 
 const Home_header = () => {
-  const {logOut,user} = useContext(inforUser)
-  const userLocal =localStorage.getItem('userCurrentlt') ? JSON.parse(localStorage.getItem('userCurrentlt')) : '';
+  // const {logOut,user} = useContext(inforUser)
+  const userLocal = localStorage.getItem("userCurrentlt")
+    ? JSON.parse(localStorage.getItem("userCurrentlt"))
+    : "";
   let navigate = useNavigate();
   const exitTodo = async () => {
     try {
-      await logOut();
- 
+      // await logOut();
     } catch (error) {
       console.log(error);
     }
-    localStorage.removeItem('userCurrentlt') // remove tài khoản dăng nhập bình thường 
+    localStorage.removeItem("userCurrentlt"); // remove tài khoản dăng nhập bình thường
   };
   // Exit todo
-  useEffect(()=>{
-    
-    if(user || userLocal ){
-      navigate("../Home", { replace: true });
-    }
-    else{
-      navigate("../", { replace: true });
-    }
+  // useEffect(()=>{
 
-  },[user,userLocal])
+  //   if(user || userLocal ){
+  //     navigate("../Home", { replace: true });
+  //   }
+  //   else{
+  //     navigate("../", { replace: true });
+  //   }
+
+  // },[user,userLocal])
 
   return (
     <Nav>
-      <S_inforUser >
+      <S_inforUser>
         <div className="imgUser">
-          <img src={user ? user.photoURL : `https://vnn-imgs-a1.vgcloud.vn/image1.ictnews.vn/_Files/2020/03/17/trend-avatar-1.jpg`} alt="" />
+          {/* <img src={user ? user.photoURL : `https://vnn-imgs-a1.vgcloud.vn/image1.ictnews.vn/_Files/2020/03/17/trend-avatar-1.jpg`} alt="" /> */}
         </div>
-        <div className="nameUser">Xin chào <span>{user ? user.displayName : userLocal.name}</span></div>
+        {/* <div className="nameUser">Xin chào <span>{user ? user.displayName : userLocal.name}</span></div> */}
       </S_inforUser>
-      <S_btnLogOut onClick={exitTodo} >
-        <ImExit   id='ImExit'  />
-       <span>Thoát</span>
+      <S_btnLogOut onClick={exitTodo}>
+        <ImExit id="ImExit" />
+        <span>Thoát</span>
       </S_btnLogOut>
     </Nav>
-  )
-}
+  );
+};
 
-export default Home_header
+export default Home_header;
--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import  Provider_day  from "./Home/Todo/Provider_day";
-import UserContext from "./Firebase/UserContext";
+import { BrowserRouter } from "react-router-dom";
+import Provider_day from "./Home/Todo/Provider_day";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <UserContext>
-
-      <Provider_day>
-        <App />
-      </Provider_day>
-      </UserContext>
-    </React.StrictMode>
+    <Provider store={store}>
+      <GoogleOAuthProvider clientId="145407773404-tk4nu9s69ilb89q6q2f9qa3gf72v3hk5.apps.googleusercontent.com">
+        <React.StrictMode>
+          <Provider_day>
+            <App />
+          </Provider_day>
+        </React.StrictMode>
+      </GoogleOAuthProvider>
+    </Provider>
   </BrowserRouter>
 );
 
